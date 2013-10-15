401c6c03fd7d09ce9a4bac0ffe81810827ea6255
Logan Yliniemi <lmy.shp@gmail.com>
2013-10-15 12:24:04 -0700

    Initial File Structure

---
+++ deep_sea_treasure/DST.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   DST.cpp
+ * Author: ylinieml
+ *
+ * Created on October 15, 2013, 12:22 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
---
+++ deep_sea_treasure/Evo_Agent_DST.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   Evo_Agent_DST.h
+ * Author: ylinieml
+ *
+ * Created on October 15, 2013, 12:23 PM
+ */
+
+#ifndef EVO_AGENT_DST_H
+#define	EVO_AGENT_DST_H
+
+
+
+#endif	/* EVO_AGENT_DST_H */
+
